3553607aaed000761082b1cca4de18535c3f65ff
Roma Sokolov <sokolov.r.v@gmail.com>
2012-09-23 22:16:01 +0400

    Init commit.
    I've got tired of copying logger.py from one eventlet-based service
    to another, and make a simple lib form it.

---
+++ eventlet_log/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from .simple import create_logger
+
+
+__all__ = ('create_logger')
+
+
+__version__ = '0.0.1'
+__author__ = 'Roma Sokolov'
+__license__ = 'MIT'
---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import eventlet_log
+
+setup(
+    name='eventlet_log',
+    version=eventlet_log.__version__,
+    description='''Damn simple logging wrapper compatible with eventlet.''',
+    long_description=open('README.rst').read(),
+    author=eventlet_log.__author__,
+    author_email='sokolov.r.v@gmail.com',
+    url='https://github.com/little-arhat/eventlet_log',
+    packages=[
+        'eventlet_log'
+    ],
+    license='MIT',
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ),
+)
